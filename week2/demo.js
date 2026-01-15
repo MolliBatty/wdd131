bb9305e94d6e7819dde1696ef6f1d6d2a6904601
Molli <molli.s.nelson@gmail.com>
2026-01-15 13:56:21 -0700

    meta tags and folder creation

--- week2/demo.js
+++
@@ -1,7 +0,0 @@
-// let tells the browser to let it exist - lets you change variable
-// never use var anymore (super old)
-// const cant change, and browser will throw an error
-
-// name - name | scope - G | Value - "Molli Batty"
-
-let name = 'molli batty';
