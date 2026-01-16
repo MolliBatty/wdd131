f0335cf9b1d9add39eea251cce4c5aaec4f21daf
Molli <molli.s.nelson@gmail.com>
2026-01-15 17:00:28 -0700

    comp thinking

---
+++ week2/ponder/compthink.js
@@ -0,0 +1,27 @@
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+        pageContent.style.color = '#003366'
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+        pageContent.style.color = '#1a4d2e'
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+        pageContent.style.color = '#654321'
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
+          
