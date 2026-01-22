30a4e7b52cae24c99afb23f4c709230e17abe84e
Molli <molli.s.nelson@gmail.com>
2026-01-22 09:26:57 -0700

    update on response

--- week3/ponder/responsive.js
+++ week3/ponder/responsive.js
@@ -1,2 +1,13 @@
+const nav = document.querySelector('nav')
+const toggleButton = document.querySelector('.menu-btn')
 
+toggleButton.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 700) {
+        nav.classList.remove('active');
+    }
+})
           
