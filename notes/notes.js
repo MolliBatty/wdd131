61888689e1aacaee583ef2c039184dcbeeeb18e2
Molli <molli.s.nelson@gmail.com>
2026-01-27 09:57:21 -0700

    week 4 ponder

---
+++ notes/notes.js
@@ -0,0 +1,30 @@
+const PI = 3.14;
+
+Math.PI
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+const subtract = (a, b) =>{
+    return a - b;
+}
+
+
+const subtract = (a, b) => a-b;
+
+ans = add(3, 6) // 9
+ans = subtract(ans, 2) //7
+// line 10 is updateing memory
+
+console.log(ans);
+
+
+// calling/invoking the function with arguments
+// arguments and parameters are the same thing but in different contexts
+// line 9 are arguments and line 1 a and b are parameters
---
+++ week4/ponder/responsivemenu.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('nav')
+const toggleButton = document.querySelector('.menu-btn')
+
+toggleButton.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 700) {
+        nav.classList.remove('active');
+    }
+})
+          
+
+const btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click', function(){
+    btn.classList.toggle('open');
+})
+
+// transition: transform 0.7s ease-in-out;
