7c6b14e061ca68016039f6db304c309244a6587a
Molli <molli.s.nelson@gmail.com>
2026-01-31 13:24:42 -0700

    coolpics2

---
+++ week4/prove/coolpics2.js
@@ -0,0 +1,45 @@
+const nav = document.querySelector('nav')
+const toggleButton = document.querySelector('.menu')
+const gallery = document.querySelector('.container');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+toggleButton.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+});
+
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if(e.target.tagName === 'IMG'){
+        const smallImageSrc = e.target.src;
+        const fullImageSrc = smallImageSrc.replace('coolpic.jpg', 'norris-full.jpg');
+        
+        modalImage.src = fullImageSrc;
+        modalImage.alt = e.target.alt;
+
+        modal.showModal();
+    }
+    
+}
+
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+     
+
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 700) {
+        nav.classList.remove('active');
+    }
+})
