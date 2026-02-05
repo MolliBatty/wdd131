16f4febeb8b9d704f78aefcd9ffd3d86e2d931a7
Molli <molli.s.nelson@gmail.com>
2026-02-05 09:49:06 -0700

    hellooooo

--- week5/ponder/access.js
+++ week5/ponder/access.js
@@ -1,2 +1,23 @@
 // go find all of the ratings and add the correct label
 // set the attribute and set the correct thing
+// parentheses indicates parameters
+
+
+// could do this
+// function addStars(span){
+//     const stars = span.innerText.length;
+//     span.setAttribute('aria-label', `${stars} out of 5 stars`);
+// }
+
+const spans = document.querySelectorAll('article.movie p span');
+
+spans.forEach((span) => {
+    const stars = span.innerText.length;
+    span.setAttribute('aria-label', `${stars} out of 5 stars`);
+});
+
+// spans.forEach(addStars);
+
+// for(let i = 0; i < spans.length; i++){
+//     addStars(spans[i]);
+// }
